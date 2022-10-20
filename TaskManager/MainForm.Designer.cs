3f5a8cbd08e7c45d32a126c50d3a547b44d07250
get458 <102791730+get458@users.noreply.github.com>
2022-10-20 17:38:42 +0300

    Добавьте файлы проекта.

---
+++ TaskManager/MainForm.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace TaskManager
+{
+    partial class TaskManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CpuBar = new System.Windows.Forms.ProgressBar();
+            this.RamBar = new System.Windows.Forms.ProgressBar();
+            this.ProMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.killToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.locationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.propertiesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SerMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ProcessTimer = new System.Windows.Forms.Timer(this.components);
+            this.PerformanceTimer = new System.Windows.Forms.Timer(this.components);
+            this.BarTimer = new System.Windows.Forms.Timer(this.components);
+            this.ProcessesPage = new System.Windows.Forms.TabPage();
+            this.ProcessGridView = new System.Windows.Forms.DataGridView();
+            this.PerformancePage = new System.Windows.Forms.TabPage();
+            this.MainList = new System.Windows.Forms.ListBox();
+            this.MainTabControl = new System.Windows.Forms.TabControl();
+            this.ProMenuStrip.SuspendLayout();
+            this.SerMenuStrip.SuspendLayout();
+            this.ProcessesPage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ProcessGridView)).BeginInit();
+            this.PerformancePage.SuspendLayout();
+            this.MainTabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CpuBar
+            // 
+            this.CpuBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CpuBar.Location = new System.Drawing.Point(12, 360);
+            this.CpuBar.Name = "CpuBar";
+            this.CpuBar.Size = new System.Drawing.Size(913, 23);
+            this.CpuBar.TabIndex = 3;
+            // 
+            // RamBar
+            // 
+            this.RamBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.RamBar.Location = new System.Drawing.Point(13, 390);
+            this.RamBar.Name = "RamBar";
+            this.RamBar.Size = new System.Drawing.Size(912, 23);
+            this.RamBar.TabIndex = 4;
+            // 
+            // ProMenuStrip
+            // 
+            this.ProMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.killToolStripMenuItem,
+            this.locationToolStripMenuItem,
+            this.propertiesToolStripMenuItem});
+            this.ProMenuStrip.Name = "ProMenuStrip";
+            this.ProMenuStrip.Size = new System.Drawing.Size(192, 70);
+            // 
+            // killToolStripMenuItem
+            // 
+            this.killToolStripMenuItem.Name = "killToolStripMenuItem";
+            this.killToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.killToolStripMenuItem.Text = "Снять задачу";
+            this.killToolStripMenuItem.Click += new System.EventHandler(this.killToolStripMenuItem_Click);
+            // 
+            // locationToolStripMenuItem
+            // 
+            this.locationToolStripMenuItem.Name = "locationToolStripMenuItem";
+            this.locationToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.locationToolStripMenuItem.Text = "Месторасположение";
+            this.locationToolStripMenuItem.Click += new System.EventHandler(this.locationToolStripMenuItem_Click);
+            // 
+            // propertiesToolStripMenuItem
+            // 
+            this.propertiesToolStripMenuItem.Name = "propertiesToolStripMenuItem";
+            this.propertiesToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.propertiesToolStripMenuItem.Text = "Подробно";
+            this.propertiesToolStripMenuItem.Click += new System.EventHandler(this.propertiesToolStripMenuItem_Click);
+            // 
+            // SerMenuStrip
+            // 
+            this.SerMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stopToolStripMenuItem,
+            this.startToolStripMenuItem,
+            this.refreshToolStripMenuItem});
+            this.SerMenuStrip.Name = "SerMenuStrip";
+            this.SerMenuStrip.Size = new System.Drawing.Size(114, 70);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            // 
+            // ProcessTimer
+            // 
+            this.ProcessTimer.Enabled = true;
+            this.ProcessTimer.Interval = 5000;
+            this.ProcessTimer.Tick += new System.EventHandler(this.ProcessTimer_Tick);
+            // 
+            // PerformanceTimer
+            // 
+            this.PerformanceTimer.Enabled = true;
+            this.PerformanceTimer.Interval = 1000;
+            this.PerformanceTimer.Tick += new System.EventHandler(this.PerformanceTimer_Tick);
+            // 
+            // BarTimer
+            // 
+            this.BarTimer.Enabled = true;
+            this.BarTimer.Interval = 10;
+            this.BarTimer.Tick += new System.EventHandler(this.BarTimer_Tick);
+            // 
+            // ProcessesPage
+            // 
+            this.ProcessesPage.Controls.Add(this.ProcessGridView);
+            this.ProcessesPage.Location = new System.Drawing.Point(4, 22);
+            this.ProcessesPage.Name = "ProcessesPage";
+            this.ProcessesPage.Padding = new System.Windows.Forms.Padding(3);
+            this.ProcessesPage.Size = new System.Drawing.Size(926, 325);
+            this.ProcessesPage.TabIndex = 1;
+            this.ProcessesPage.Text = "Processes";
+            this.ProcessesPage.UseVisualStyleBackColor = true;
+            // 
+            // ProcessGridView
+            // 
+            this.ProcessGridView.AllowUserToAddRows = false;
+            this.ProcessGridView.AllowUserToDeleteRows = false;
+            this.ProcessGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ProcessGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.ProcessGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.ProcessGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ProcessGridView.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.ProcessGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ProcessGridView.ContextMenuStrip = this.ProMenuStrip;
+            this.ProcessGridView.Location = new System.Drawing.Point(3, 3);
+            this.ProcessGridView.Name = "ProcessGridView";
+            this.ProcessGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.ProcessGridView.Size = new System.Drawing.Size(920, 319);
+            this.ProcessGridView.TabIndex = 0;
+            // 
+            // PerformancePage
+            // 
+            this.PerformancePage.Controls.Add(this.MainList);
+            this.PerformancePage.Location = new System.Drawing.Point(4, 22);
+            this.PerformancePage.Name = "PerformancePage";
+            this.PerformancePage.Padding = new System.Windows.Forms.Padding(3);
+            this.PerformancePage.Size = new System.Drawing.Size(926, 325);
+            this.PerformancePage.TabIndex = 0;
+            this.PerformancePage.Text = "Performance";
+            this.PerformancePage.UseVisualStyleBackColor = true;
+            // 
+            // MainList
+            // 
+            this.MainList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MainList.FormattingEnabled = true;
+            this.MainList.Location = new System.Drawing.Point(0, 0);
+            this.MainList.Name = "MainList";
+            this.MainList.Size = new System.Drawing.Size(926, 329);
+            this.MainList.TabIndex = 1;
+            // 
+            // MainTabControl
+            // 
+            this.MainTabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MainTabControl.Controls.Add(this.PerformancePage);
+            this.MainTabControl.Controls.Add(this.ProcessesPage);
+            this.MainTabControl.Location = new System.Drawing.Point(3, 3);
+            this.MainTabControl.Multiline = true;
+            this.MainTabControl.Name = "MainTabControl";
+            this.MainTabControl.SelectedIndex = 0;
+            this.MainTabControl.Size = new System.Drawing.Size(934, 351);
+            this.MainTabControl.TabIndex = 7;
+            // 
+            // TaskManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 450);
+            this.Controls.Add(this.MainTabControl);
+            this.Controls.Add(this.RamBar);
+            this.Controls.Add(this.CpuBar);
+            this.Name = "TaskManager";
+            this.Text = "Диспетчер задач";
+            this.ProMenuStrip.ResumeLayout(false);
+            this.SerMenuStrip.ResumeLayout(false);
+            this.ProcessesPage.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ProcessGridView)).EndInit();
+            this.PerformancePage.ResumeLayout(false);
+            this.MainTabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ProgressBar CpuBar;
+        private System.Windows.Forms.ProgressBar RamBar;
+        private System.Windows.Forms.ContextMenuStrip ProMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem killToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem locationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem propertiesToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip SerMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+        private System.Windows.Forms.Timer ProcessTimer;
+        private System.Windows.Forms.Timer PerformanceTimer;
+        private System.Windows.Forms.Timer BarTimer;
+        private System.Windows.Forms.TabPage ProcessesPage;
+        public System.Windows.Forms.DataGridView ProcessGridView;
+        private System.Windows.Forms.TabPage PerformancePage;
+        private System.Windows.Forms.ListBox MainList;
+        private System.Windows.Forms.TabControl MainTabControl;
+    }
+}
+
---
+++ TaskManager/MainForm.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace TaskManager
+{
+    public partial class TaskManager : Form
+    {
+        public TaskManager()
+        {
+            
+            InitializeComponent();
+            Processes.CreateProcessList();
+            ProcessGridView.DataSource = Globals.ProTable;
+            Processes.RefreshProcessList();
+            Processes.UpdateProcessList();
+            ProcessGridView.Update();
+        }
+
+        public static class Globals
+        {
+            public static string Name { get; set; } = "";
+            public static int LoadPercentage { get; set; } = 0;
+            public static int CurrentClockSpeed { get; set; } = 0;
+            public static int MaxClockSpeed { get; set; } = 0;
+            public static int NumberOfCores { get; set; } = 0;
+            public static int NumberOfLogicalProcessors { get; set; } = 0;
+            public static int L2CacheSize { get; set; } = 0;
+            public static int FreePhysicalMemory { get; set; } = 0;
+            public static int TotalVisibleMemorySize { get; set; } = 0;
+            public static int TotalVirtualMemorySize { get; set; } = 0;
+            public static int FreeVirtualMemory { get; set; } = 0;
+            public static int NumberOfProcessors { get; set; } = 0;
+            public static int ProID { get; set; } = 0;
+            public static DataTable ProTable { get; set; } = new DataTable();
+            public static DataTable SerTable { get; set; } = new DataTable();
+            public static Bitmap Icon { get; set; } = new Bitmap(1,1);
+            public static int[] CPU { get; set; } = Enumerable.Repeat(0, 60).ToArray(); 
+            public static int[] RAM { get; set; } = Enumerable.Repeat(0, 60).ToArray();
+
+            public static int[] Time { get; set; } = {
+                -60, -59, -58, -57, -56, -55, -54, -53, -52, -51, -50, -49, -48, -47, -46,
+                -45, -44, -43, -42, -41, -40, -39, -38, -37, -36, -35, -34, -33, -32, -31,
+                -30, -29, -28, -27, -26, -25, -24, -23, -22, -21, -20, -19, -18, -17, -16,
+                -15, -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1};
+        }
+
+        public void PopMainList()
+        {
+            MainList.Items.Clear();
+            MainList.Items.Add("Название: " + Globals.Name);
+            MainList.Items.Add("Загрузка: " + Globals.LoadPercentage + "%");
+            MainList.Items.Add("Тактовая частота: " + Globals.CurrentClockSpeed);
+            MainList.Items.Add("Количество ядер: " + Globals.NumberOfCores);
+            MainList.Items.Add("Количество потоков: " + Globals.NumberOfLogicalProcessors);
+            MainList.Items.Add("Объем RAM (ГБ): " + Globals.TotalVisibleMemorySize / 1048576);
+        }
+
+        //бары для загрузки
+        public void PopBars()
+        {
+            CpuBar.Visible = true;
+            CpuBar.Maximum = 100;
+            CpuBar.Value = Globals.CPU[Globals.CPU.Length - 1];
+
+            RamBar.Visible = true;
+            RamBar.Minimum = 1;
+            RamBar.Maximum = Globals.TotalVisibleMemorySize;
+            RamBar.Value = Globals.TotalVisibleMemorySize - Globals.FreePhysicalMemory;
+        }
+
+        //снять задачу
+        private void killToolStripMenuItem_Click(object sender, EventArgs e)
+        {           
+            try
+            {
+                Process proc = Process.GetProcessById(Int32.Parse(ProcessGridView.CurrentRow.Cells[1].Value.ToString()));
+                proc.Kill();
+                Processes.RefreshProcessList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        //местоположение
+        private void locationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process proc = Process.GetProcessById(Int32.Parse(ProcessGridView.CurrentRow.Cells[1].Value.ToString()));
+                string fullPath = proc.MainModule.FileName;
+                Process.Start("explorer.exe", "/select, " + fullPath);
+                Processes.RefreshProcessList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+
+        //свойства
+        private void propertiesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process proc = Process.GetProcessById(Int32.Parse(ProcessGridView.CurrentRow.Cells[1].Value.ToString()));
+                string fullPath = proc.MainModule.FileName;
+                ViewProperties.ViewProperties.ShowFileProperties(fullPath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+
+        private void ProcessTimer_Tick(object sender, EventArgs e)
+        {
+            if (MainTabControl.SelectedTab == MainTabControl.TabPages["ProcessesPage"])
+            {
+                Processes.UpdateProcessList();
+                ProcessGridView.Update();
+            }
+        }
+
+        private void PerformanceTimer_Tick(object sender, EventArgs e)
+        {
+            Task.Factory.StartNew(() => Win32.Win32_Processor());
+            Task.Factory.StartNew(() => Win32.Win32_OperatingSystem());
+            Task.Factory.StartNew(() => Win32.Win32_ComputerSystem());
+
+            if (MainTabControl.SelectedTab == MainTabControl.TabPages["PerformancePage"])
+            {
+                PopMainList();
+            }
+        }
+
+        //полосы загрузок
+        private void BarTimer_Tick(object sender, EventArgs e)
+        {
+            PopBars();
+
+            using (Graphics Draw = CpuBar.CreateGraphics())
+            {
+                string CPU = "CPU " + Globals.CPU[Globals.CPU.Length - 1].ToString() + "%";
+                Draw.DrawString(CPU, new Font(FontFamily.GenericSansSerif, 12.0F, FontStyle.Regular, GraphicsUnit.Pixel, 128), Brushes.Black, (CpuBar.Width / 2 - CPU.Length * 2), (CpuBar.Height / 2 - 7));
+            }
+
+            using (Graphics Draw = RamBar.CreateGraphics())
+            {
+                string RAM = "RAM " + Globals.RAM[Globals.RAM.Length - 1].ToString() + "%" + '\t' + (Globals.FreePhysicalMemory / 1024) + " МБ" + " / " + (Globals.TotalVisibleMemorySize / 1024) + " МБ";
+                Draw.DrawString(RAM, new Font(FontFamily.GenericSansSerif, 12.0F, FontStyle.Regular, GraphicsUnit.Pixel, 128), Brushes.Black, (CpuBar.Width / 2 - RAM.Length * 2), (CpuBar.Height / 2 - 7));
+            }
+        }
+    }
+}
+
